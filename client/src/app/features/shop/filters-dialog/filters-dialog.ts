afc046d142a701561e937714c40fa5e1cb92d5d6
aissan <aysanm1889@gmail.com>
2025-12-10 01:56:21 +0330

    Update json files and images

--- client/src/app/features/shop/filters-dialog/filters-dialog.ts
+++ client/src/app/features/shop/filters-dialog/filters-dialog.ts
@@ -1,42 +1,38 @@
 import { Component, inject } from '@angular/core';
-import { ShopService } from '../../../core/services/ShopService';
-import {MatDivider} from '@angular/material/divider'
-import {MatListOption, MatSelectionList} from '@angular/material/list'
-import { MatButton } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { ShopService } from '../../../core/services/ShopService';
+
 @Component({
   selector: 'app-filters-dialog',
+  standalone: true,
   imports: [
     CommonModule,
-    MatDivider,
-    MatSelectionList,
-    MatListOption,
-    MatButton,
-    FormsModule
+    FormsModule,
+    MatDividerModule,
+    MatListModule,
+    MatButtonModule
   ],
   templateUrl: './filters-dialog.html',
-  styleUrl: './filters-dialog.scss',
+  styleUrls: ['./filters-dialog.scss']
 })
 export class FiltersDialog {
 
   shopService = inject(ShopService);
-
   private dialogRef = inject(MatDialogRef<FiltersDialog>);
-
   data = inject(MAT_DIALOG_DATA);
 
-  selectedBrands: string[] = this.data.selectedBrands;
-  selectedGroups: string[] = this.data.selectedGroups;
+  selectedBrands: string[] = this.data.selectedBrands || [];
+  selectedGroups: string[] = this.data.selectedGroups || [];
 
   applyFilters() {
-
     this.dialogRef.close({
       selectedBrands: this.selectedBrands,
       selectedGroups: this.selectedGroups
-    })
-
+    });
   }
-
 }
